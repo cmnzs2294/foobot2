5239b4c548cfd29c7e8194cb698134069b55f5d3
unknown <Nina@Carolina.mit.edu>
2023-10-18 17:45:28 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  // Handle new websocket connection
+  console.log('New client connected');
+
+  // Broadcast incoming messages to all connected clients
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  // Handle client disconnection
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(8080, () => {
+  console.log('Server is listening on port 8080');
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Background } from "./components/Background";
 import { OrbitControls } from "@react-three/drei";
@@ -15,6 +15,55 @@ function App() {
         <Background />
         <Cubes />
 
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
+
+*/
+
+import React, { useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Background } from "./components/Background";
+import { OrbitControls } from "@react-three/drei";
+import { Cubes } from "./components/Cubes";
+
+function App() {
+  useEffect(() => {
+    const socket = new WebSocket('wss://f oobotgame.glitch.me');  // Update with your server URL
+
+    // Listen for messages from the server
+    socket.addEventListener('message', (event) => {
+      console.log('Message from server:', event.data);
+      // Handle messages received from the server
+    });
+
+    return () => {
+      // Clean up websocket on component unmount
+      socket.close();
+    };
+  }, []);
+
+  return (
+    <>
+      <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
+        {function App() {
+  return (
+    <>
+      <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
+        <color attach="background" args={["#white"]} />
+
+        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2.15} />
+
+        <Background />
+        <Cubes />
+
+      </Canvas>
+    </>
+  );
+}}
       </Canvas>
     </>
   );
