d1d5cccecd3dbf7415dcb48ca27c90dd133808de
carolina <cmnzs@mit.edu>
2023-10-19 17:30:43 -0400

    set up socket on client

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,4 @@
-/* import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { Background } from "./components/Background";
-import { OrbitControls } from "@react-three/drei";
-import { Cubes } from "./components/Cubes";
-
-function App() {
-  return (
-    <>
-      <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
-        <color attach="background" args={["#white"]} />
-
-        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2.15} />
-
-        <Background />
-        <Cubes />
-
-      </Canvas>
-    </>
-  );
-}
-
-export default App;
-
-v2 below
-
-import React, { useEffect } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Background } from "./components/Background";
-import { OrbitControls } from "@react-three/drei";
-import { Cubes } from "./components/Cubes";
-
-function App() {
-  useEffect(() => {
-    const socket = new WebSocket('wss://foobotgame.glitch.me');  // Update with your server URL
-
-    // Listen for messages from the server
-    socket.addEventListener('message', (event) => {
-      console.log('Message from server:', event.data);
-      // Handle messages received from the server
-    });
-
-    return () => {
-      // Clean up websocket on component unmount
-      socket.close();
-    };
-  }, []);
-
-  return (
-    <>
-      <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
-        {function App() {
-  return (
-    <>
-      <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
-        <color attach="background" args={["#white"]} />
-
-        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2.15} />
-
-        <Background />
-        <Cubes />
-
-      </Canvas>
-    </>
-  );
-}}
-      </Canvas>
-    </>
-  );
-}
-
-export default App;
-*/
-
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Background } from "./components/Background";
 import { OrbitControls } from "@react-three/drei";
@@ -82,9 +8,31 @@ function App() {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
+    // Create a WebSocket connection when the component mounts
     const newSocket = new WebSocket('wss://foobotgame.glitch.me'); // Replace with your server URL
+
+    // Set up event listeners for the WebSocket
+    newSocket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    newSocket.onmessage = (event) => {
+      // Handle incoming messages from the server
+      console.log('Received message from server:', event.data);
+    };
+
+    newSocket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    newSocket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    // Save the socket in the component state
     setSocket(newSocket);
 
+    // Clean up the socket when the component unmounts
     return () => {
       if (newSocket) {
         newSocket.close();
@@ -92,14 +40,15 @@ function App() {
     };
   }, []);
 
-
   return (
-    <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
-      <color attach="background" args={["white"]} />
-      <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2.15} />
-      <Background />
-      <Cubes />
-    </Canvas>
+    <>
+      <Canvas camera={{ fov: 45, position: [-10, 10, 10] }}>
+        <color attach="background" args={["#white"]} />
+        <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2.15} />
+        <Background />
+        <Cubes />
+      </Canvas>
+    </>
   );
 }
 
