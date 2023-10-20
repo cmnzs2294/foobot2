bb372f88a93cbaa387eadeeca5399e331650f4b9
carolina <cmnzs@mit.edu>
2023-10-20 10:48:45 -0400

    game state in server

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const WebSocket = require('ws');
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
+const gameState = {
+  currentPlayer: 1,
+  cubes: [],
+};
 
 
 let connectedClients = 0;
@@ -55,34 +58,6 @@ wss.on('connection', (ws) => {
 
 
 
-/* player counter
-
-let clientIdCounter = 0; // Initialize the client ID counter
-
-wss.on('connection', (ws) => {
-  // Increment the counter for each new connection to assign a unique client ID
-  clientIdCounter++;
-
-  // Assign the unique client ID to the connected client
-  ws.clientId = clientIdCounter;
-
-  // Handle new websocket connection and log the client ID
-  console.log(`Client ${ws.clientId} connected`);
-
-  // Broadcast incoming messages to all connected clients
-  ws.on('message', (message) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
-
-  // Handle client disconnection and log the client ID
-  ws.on('close', () => {
-    console.log(`Client ${ws.clientId} disconnected`);
-  });
-}); */
 
 server.listen(8080, () => {
   console.log('Server is listening on port 8080');
